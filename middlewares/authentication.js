4d138c0be5ef07acb8141ccce8ed4b4169802792
kuttu101996 <kuttulr1996@gmail.com>
2023-08-24 21:07:53 +0530

    authentication.js change

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken")
 
 require("dotenv").config();
 const authentication = (req, res, next) => {
-  const token = req.cookies.token;
-// const token = req.headers.authorization
+  // const token = req.cookies.token;
+const token = req.headers.authorization
   try {
     jwt.verify(token, process.env.secret_key, async function (err, decoded) {
       if (err) {
